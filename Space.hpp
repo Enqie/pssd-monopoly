5dcdf94a66f72a9bc6b46610143a7da0f41649ca
kimmy <gancaiyuin@gmail.com>
2025-09-19 12:16:06 +0930

    initial implementation for abstract class Space

--- Space.hpp
+++ Space.hpp
@@ -5,11 +5,15 @@
 class Space
 {
 protected:
-    /* data */
-    std::string name;   // name of space
-    bool canBuy;        // can space be bought by player
-    int cost;           // cost of landing on this space
+    std::string name;       // name of space
+    bool canBuy;            // can space be bought by player
+    int cost;               // cost of landing on this space
+    bool isOwned = false;   // bool for if space is owned? 
+    bool hasEffect;         // for jail or chance/community chest - if true then can call function?
 public:
     Space(/* args */){}
-    Space(std::string name): name(name){}
+    Space(std::string name, bool canBuy, int cost, bool hasEffect): name(name), canBuy(canBuy), cost(cost), hasEffect(hasEffect) {}
+    
+    int getCost(){ return cost; }
+    void setCost(int newCost){ cost = newCost; }
 };
