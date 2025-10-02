a22ef132921a90f3f343e3b2b03b81fedd3df34e
kimmy <gancaiyuin@gmail.com>
2025-10-03 00:49:44 +0930

    updated Space.hpp

--- Space.hpp
+++ Space.hpp
@@ -5,15 +5,11 @@
 class Space
 {
 protected:
-    std::string name;       // name of space
-    bool canBuy;            // can space be bought by player
-    int cost;               // cost of landing on this space
-    bool isOwned = false;   // bool for if space is owned? 
-    bool hasEffect;         // for jail or chance/community chest - if true then can call function?
+    std::string name;           // name of space
 public:
+    // constructors
     Space(/* args */){}
-    Space(std::string name, bool canBuy, int cost, bool hasEffect): name(name), canBuy(canBuy), cost(cost), hasEffect(hasEffect) {}
-    
-    int getCost(){ return cost; }
-    void setCost(int newCost){ cost = newCost; }
+    Space(std::string name) {}
+
+    virtual void land() = 0;    // pure virtual function for what happens if you land on a space
 };
