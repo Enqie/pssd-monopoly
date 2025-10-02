c0a2294f386b7d8232b6d79b04ac48babf74b006
kimmy <gancaiyuin@gmail.com>
2025-10-03 01:43:30 +0930

    updated player class

--- Player.hpp
+++ Player.hpp
@@ -10,6 +10,12 @@ class Player
 private:
     std::string name;
 
+    int money = 1500;           // player starts with 1500
+
+    // booleans for player state
+    bool isInJail = false;
+    bool isBankrupt = false;
+
     // container to store Properties owned by player
     std::unordered_set<Property*> ownedProperties;
 
@@ -18,6 +24,13 @@ private:
 public:
     Player(std::string name): name(name){}
 
+    // getters/setters
     std::string getName(){ return name; }
+    int getMoney(){ return money; }
     std::unordered_set<Property*> getProperties(){ return ownedProperties; }
+    void setMoney(int newAmount){ money = newAmount; }
+
+    // money functions
+    void addMoney(int amount){ money+=amount; }
+    void subMoney(int amount){ money-=amount; }
 };
