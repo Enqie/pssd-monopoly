2d47f341bd66c0ded82c18d3d01509c5df8f08e9
kimmy <gancaiyuin@gmail.com>
2025-10-08 00:28:29 +1030

    NEED TO ADD LOGIC FOR LAND FUNCTION

---
+++ GoToJail.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Space.hpp"
+
+class GoToJail : public Space
+{
+private:
+    
+public:
+    GoToJail(): Space("Go To Jail"){}
+
+    void land(Player* player){
+        /* LOGIC FOR GO TO JAIL */
+    }
+};
