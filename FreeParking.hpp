407dd693f552cfcf9999311bf53d2013ef998880
kimmy <gancaiyuin@gmail.com>
2025-10-07 23:56:54 +1030

    created class for free parking space including necessary logic for when player lands on space

---
+++ FreeParking.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Space.hpp"
+
+class FreeParking : public Space
+{
+public:
+    FreeParking(std::string name): Space(name){}
+
+    void land(Player* player){ return; }
+};
